5154ce30ed57e20b43a43b60dcb2b028aa70b7b6
Heyscar Romero <heyscarromero@gmail.com>
2024-10-17 19:48:46 -0300

    Refactor email sender address

--- src/emails/actions/sendEmailToContact.ts
+++ src/emails/actions/sendEmailToContact.ts
@@ -5,7 +5,7 @@ import { ClientEmailTemplate } from "../components/ClientMailTemplate";
 import { Message } from "@/interfaces";
 
 export const sendEmailToContact = async (message: Message) => {
-  const emailFrom = "heyscar.dev@resend.dev";
+  const emailFrom = "heyscar@heyscar.dev";
   const resend = new Resend(process.env.NEXT_RESEND_API_KEY);
   const { data, error } = await resend.emails.send({
     from: emailFrom,
